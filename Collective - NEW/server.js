8286d5c507235ad9467602c78e4ab128663635e3
Sam Daviies <sam.davies-24@students.plymouth.ac.uk>
2023-01-20 13:31:48 +0000

    Code Tidy and comments

--- Collective - NEW/server.js
+++ Collective - NEW/server.js
@@ -8,13 +8,12 @@ const express = require('express');
 const app = express();
 const server = require("http").createServer(app);
 const bodyParser = require('body-parser');
-//var sessionstore = require('sessionstore');
-var ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 //Login handler modules
 const LocalStrategy = require('passport-local').Strategy
 const flash = require('express-flash');
-let session = require('express-session');
+const session = require('express-session');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 
@@ -43,9 +42,6 @@ app.use((req, res, next) => {
     req.io = io;
     return next();
   });
-
-
-
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -60,7 +56,7 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
     console.log('Connected to MongoDB');
 
     //Define database and collections
-    let dbo = db.db('collective')
+    let dbo = db.db('test')
     let chats = dbo.collection('chats');
     let users = dbo.collection('users');
     let sessions = dbo.collection('sessions');
@@ -100,48 +96,41 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
         )
 
     
-
+    //API Routes
     app.get('/', checkAuthenticated, async (req, res) => {
+
         let usr = await getUserById(req.session.passport.user)
         res.render("index", {name: usr})
 
         });
 
-    app.get('/login', checkNotAuthenticated, (req, res, socket) => {
-           
-            console.log("No session", socket.id)
+    app.get('/login', checkNotAuthenticated, (req, res) => {
 
         res.render("login", {message: 'Please enter your email and password'})
         });
 
+    app.post('/login',checkNotAuthenticated, passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}), function(req, res) {   
 
-    app.post('/login',
-     checkNotAuthenticated,
-      passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}),
-      function(req, res) {   
         res.render("index", {name: req.user.name})
-        saveUser(req)
-              
-    });
+        });
 
 
     app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render("register.ejs")
-    });
+
+        res.render("register.ejs")
+        });
 
     app.post('/register', checkNotAuthenticated, async (req, res) => {
+
        try{
             const hashedPassword = await bcrypt.hash(req.body.password, 10)
-            //console.log( hashedPassword)
             users.insertOne({name: req.body.username, email: req.body.email, password: hashedPassword, friends: []}, function(){
                 res.redirect('/login')
             })
-
         }catch(err){
             res.redirect('/register')
-            console.log(err);
         }
-    });
+        });
 
     app.get('/users', async (req, res) => {
         
@@ -151,21 +140,17 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
                 res.json(result[0])
 
             })
-        } catch (error) {
-            
-        }
+        } catch (error) {}
+        });
 
-    })
-
-      app.get("/chats", async (req, res) => {
+    app.get("/chats", async (req, res) => {
         try {
             let usr = await users.find({"_id" : ObjectId(req.session.passport.user)}).toArray()
             chats.find({members: usr[0].name}).toArray(function(err, result) {
              if (err) throw err;
              res.json(result)
          });  
-        } catch (error) {
-        }
+        } catch (error) {}
       });
 
     app.get("/logout", (req, res) => {
@@ -174,22 +159,24 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
           
           res.redirect("/login");
         });
-      });
+    });
+
       
-      function checkAuthenticated(req, res, next) {
-        if (req.isAuthenticated()) {
-          return next()
-        }
-      
-        res.redirect('/login')
-      }
-      
-      function checkNotAuthenticated(req, res, next) {
-        if (req.isAuthenticated()) {
-          return res.redirect('/')
-        }
-        next()
-      }
+    //Authentication check functions
+    function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    
+    res.redirect('/login')
+    }
+    
+    function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+    }
 
 
     //Socket.io
@@ -197,48 +184,41 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
 
         let chats = dbo.collection('chats');
         let messages = dbo.collection('messages');
-        //let user = await getUser()
             try{
                 console.log('Connected to Socket.io, socket id: ' + socket.id);
 
                 messages.find({}).toArray(function(err, result) {
                     if (err) throw err;
-
                 }); 
 
-            }catch(err){
-                console.log(err);
-            }  
+            }catch(err){}  
             
-            //Get chat list
-            socket.on('receiveChats', async (chat) => {
-                let user = await getUser(socket.id)
-                chats.find( { members :{ $elemMatch :{name : user} }}).toArray(function(err, result) {
-                     if (err) throw err;
-                io.emit('receiveChats', [chat])
-                });
+        //Get chat list
+        socket.on('receiveChats', async (chat) => {
+            let user = await getUser(socket.id)
+            chats.find( { members :{ $elemMatch :{name : user} }}).toArray(function(err, result) {
+                    if (err) throw err;
+            io.emit('receiveChats', [chat])
             });
-
-
-    socket.on('loggedIn', async (user, sid) => {
-        try {
-            users.updateOne({"name" : user.name }, {$set: {socketId: sid}})
-        } catch (error) {
-            
-        }
-        
         });
+
+
+        //Set socket id to user
+        socket.on('loggedIn', async (user, sid) => {
+            try {
+                users.updateOne({"name" : user.name }, {$set: {socketId: sid}})
+            } catch (error) {}
+            
+            });
 
         //Join Chat
         socket.on('joinRoom', async function(room){
             var rooms = await Array.from(socket.rooms);
             socket.leave(rooms[1])
             socket.join(room)
-            //console.log(room)
 
             messages.find({roomName: room}).toArray(function(err, result) {
                 if (err) throw err;
-                //console.log(result)
                 io.to(socket.id).emit('receiveMessage', result)
             });
             })
@@ -251,202 +231,101 @@ mongodb.connect('mongodb://localhost:27017', (err, db) => {
             });
         })
 
-        //Join chat
-        socket.on('loadMessages', async (chat) => {
-            messages.find({chat: chat.name}).toArray(function(err, result) {
+
+        //Add friend
+        socket.on('addFriend', async (user, friend) => {
+
+            //Does the friend exist?
+            users.findOne({name: friend}, function(err, result) {
                 if (err) throw err;
-                io.emit('receiveMessage', result)
-
-                messages.find({name: { $in: chat.members}}).toArray(function(err, result) {
-                    if (err) throw err;
-                    // io.emit('receiveChats', [chat])
-                    result.forEach(element => {
-                        io.to(element.socketId).emit('receiveChats', [chat])
-                    });
-                });
-
-
+                    if(result){
+                        //Does the user already have the friend?
+                        users.findOne({name: user, friends: friend}, function(err, result) {
+                            if (err) throw err;
+                                if(result){
+                                    io.to(socket.id).emit('friendAdded', friend + " is already a friend")
+                                }else{
+                                    users.updateOne({"name" : user}, {$push: {friends: friend}})
+                                    io.to(socket.id).emit('friendAdded', friend + " added as a friend")
+                                }
+                        });
+                    }else{
+                        io.to(socket.id).emit('friendAdded', friend +  " does not exist")
+                    }
             });
-        })
-
-
-    socket.on('addFriend', async (user, friend) => {
-
-        //Does the friend exist?
-        users.findOne({name: friend}, function(err, result) {
-            if (err) throw err;
-                if(result){
-                    //Does the user already have the friend?
-                    users.findOne({name: user, friends: friend}, function(err, result) {
-                        if (err) throw err;
-                            if(result){
-                                io.to(socket.id).emit('friendAdded', "Already a friend")
-                            }else{
-                                users.updateOne({"name" : user}, {$push: {friends: friend}})
-                                io.to(socket.id).emit('friendAdded', "Friend added")
-                            }
-                    });
-                }else{
-                    io.to(socket.id).emit('friendAdded', "User does not exist")
-                }
         });
-    });
 
-                
-  
+        //Add chat
+        socket.on('addChat', async (chat) => {
 
-
-
-
-    socket.on('addChat', async (chat) => {
-        console.log("Name: ", chat.name)
-        chats.findOne({name: chat.name}, function(err, result) {
-            console.log("Result: ", result)
-            if (err) throw err;
-                if(result){
-                    console.log("Chatroom name already exists")
-                    io.to(socket.id).emit('chatAdded', "Chatroom name already exists")
-                }else{
-                    chats.insertOne({owner: chat.user.name, name: chat.name, members: chat.members})
-                    io.to(socket.id).emit('chatAdded', "Chat added")
-                    io.to(socket.id).emit('receiveChats', [chat])
-                }
+            //Does the chat already exist?
+            chats.findOne({name: chat.name}, function(err, result) {
+                if (err) throw err;
+                    if(result){
+                        io.to(socket.id).emit('chatAdded', "Chatroom name already exists")
+                    }else{
+                        chats.insertOne({owner: chat.user.name, name: chat.name, members: chat.members})
+                        users.find({name: { $in: chat.members}}).toArray(function(err, result) {
+                            if (err) throw err;
+                            result.forEach(element => {
+                                io.to(element.socketId).emit('receiveChats', [chat])
+                            });
+                        });
+                    }
+            });
         });
-    });
 
 
-
-        // chats.insertOne({owner: chat.user.name, name: chat.name, members: chat.members})
-        // users.find({name: { $in: chat.members}}).toArray(function(err, result) {
-        //     if (err) throw err;
-        //     // io.emit('receiveChats', [chat])
-        //     result.forEach(element => {
-        //         io.to(element.socketId).emit('receiveChats', [chat])
-        //     });
-        // });
-
-
-        //         //Does the friend exist?
-        //         users.findOne({name: friend}, function(err, result) {
-        //             if (err) throw err;
-        //                 if(result){
-        //                     //Does the user already have the friend?
-        //                     users.findOne({name: user, friends: friend}, function(err, result) {
-        //                         if (err) throw err;
-        //                             if(result){
-        //                                 io.to(socket.id).emit('friendAdded', "Already a friend")
-        //                             }else{
-        //                                 users.updateOne({"name" : user}, {$push: {friends: friend}})
-        //                                 io.to(socket.id).emit('friendAdded', "Friend added")
-        //                             }
-        //                     });
-        //                 }else{
-        //                     io.to(socket.id).emit('friendAdded', "User does not exist")
-        //                 }
-        //         });
-
-
-
-        //Send Messages
+        //Send Message
         socket.on('sendMessage', async (msg, req) => {
-            // let sender = msg.sender;
-            // let message = msg.message;
-            // let date = msg.date;
-            // let time = msg.time;
+
             var room = await Array.from(socket.rooms);
-            console.log("test", room[1])
             try{
                 messages.insertOne({sender: msg.sender, roomName: room[1], message: msg.message, date: msg.date, time: msg.time}, function(){
                     io.to(room[1]).emit('receiveMessage', [msg])
                 })            
-             }
-            catch(err){
-                console.log(err);
-            }
-    });
+                }
+            catch(err){}
+        });
 
-        
-    async function getUser(sid){
-        try {
-            users.findOne({socketId: sid}, function(err, result) {
-                if (err) throw err;
-                return result
-            })
-
-        } catch (error) {
-            
+        //Get User by socket id
+        async function getUser(sid){
+            try {
+                users.findOne({socketId: sid}, function(err, result) {
+                    if (err) throw err;
+                    return result
+                })
+            } catch (error) {}
         }
-    }
 
-        //Get Messages
-
-        // async function getMessages(chat){
-        //     try {
-        //         //console.log(chat)
-        //         await messages.find({roomName: chat}).toArray(function(err, result) {
-        //             if (err) throw err;
-        //             io.to(socket.rooms[1]).emit('receiveMessage', messages)
-        //             //io.to(socket.rooms[1]).emit('receiveMessage', result)
-        //             //console.log(socket.id)
-        //             return result
-        //         });
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // }
-
-
-        //Status function
-        showStatus = function(s) {
-            socket.emit('status', s);
-        }
     });
 
 
-
+    //Get user by email
     async function getUserByEmail(email){
         try {
             let result = await users.find({email: email}).toArray();
-            //console.log(result[0].name)
-            //let query = users.find({email : email})
             return result[0];
-        } catch (error) {
-            console.log(error);
-        }
+        } catch (error) {}
 
       }
 
-      async function getUserById(id){
-        //console.log(id)
+    //Get user by id
+    async function getUserById(id){
+
        try {
-        
-        users.find({"_id" : ObjectId(id)}).toArray(async function(err, result) {
-            if (err) throw err;
-            //console.log(result[0].name)
-            return result[0]
+            users.find({"_id" : ObjectId(id)}).toArray(async function(err, result) {
+                if (err) throw err;
+                return result[0]
+            })
+       } 
+       catch (error) {}
+    }
 
-        })
-        //console.log(user)
-
-       } catch (error) {
-        
-       }
-
-        //console.log(result)
-        //console.log(result[0])
-        //let query = users.find({email : email})
-        
-      }
-      function saveUser(req){
-        // passport.user = req.user;
-        // let session = req.session;
-        // console.log
-      }
-      
+          
     server.listen(3001, () => {
         console.log('Server is running on port 3001');
       });
       
 });
 
-module.exports = app;
